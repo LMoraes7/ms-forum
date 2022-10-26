b95e24bfc01f8a25d296feaf9791c7cf8b99ccef
Moraes <lukaocavalcante93@gmail.com>
2022-10-26 04:47:04 -0300

    refatorando

--- src/main/java/br/com/forum/domain/service/GeneratorIdentifierService.java
+++ src/main/java/br/com/forum/domain/service/GeneratorIdentifierService.java
@@ -7,26 +7,20 @@ import java.util.UUID;
 @Service
 public final class GeneratorIdentifierService {
 
-    private final String USER_PREFIX = "USR-";
-    private final String TOPIC_PREFIX = "TOP-";
-    private final String COMMENT_PREFIX = "COM-";
-
     public String userId() {
-        final String[] split = generateArrayUuid();
-        return USER_PREFIX.concat(split[1]).concat(split[2]);
+        return "USR-".concat(generateHash());
     }
 
     public String topicId() {
-        final String[] split = generateArrayUuid();
-        return TOPIC_PREFIX.concat(split[1]).concat(split[2]);
+        return "TOP-".concat(generateHash());
     }
 
     public String commentId() {
-        final String[] split = generateArrayUuid();
-        return COMMENT_PREFIX.concat(split[1]).concat(split[2]);
+        return "COM-".concat(generateHash());
     }
 
-    private static String[] generateArrayUuid() {
-        return UUID.randomUUID().toString().split("-");
+    private String generateHash() {
+        final var split = UUID.randomUUID().toString().split("-");
+        return split[1].concat(split[2]);
     }
 }
